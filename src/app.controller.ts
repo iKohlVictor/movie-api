86dc617043e9e2e9a13603b054103b3aad1a93ca
ikohlvictor <ianvictor181099@gmail.com>
2025-04-23 22:07:25 -0300

    download file with streaming

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Controller, Get, Query, Res } from "@nestjs/common";
 import { AppService } from "./app.service";
+import { Response } from "express";
+import { WriteStream } from "fs";
 
 @Controller()
 export class AppController {
@@ -8,5 +10,34 @@ export class AppController {
   @Get()
   getMovies() {
     return this.appService.getMovies();
+  }
+
+  @Get("movies/:format")
+  async getMoviesInFormatExcelOrCSV(
+    @Query("format") format: string,
+    @Res() res: Response
+  ) {
+    const filename = format === "xlsx" ? "movies.xlsx" : "movies.csv";
+    if (format === "xlsx") {
+      res.set({
+        "Content-Type":
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        "Content-Disposition": `attachment; filename="${filename}"`,
+      });
+    } else {
+      res.set({
+        "Content-Type": "text/csv",
+        "Content-Disposition": `attachment; filename="${filename}"`,
+      });
+    }
+
+    if (format === "xlsx") {
+      await this.appService.getMoviesInFormatExcel(
+        res as unknown as WriteStream
+      );
+    } else {
+      await this.appService.getMoviesInFormatCSV(res as unknown as WriteStream);
+    }
+    res.end();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,23 +1,19 @@
 import { Injectable } from "@nestjs/common";
-import { Readable } from "stream";
+import { Readable, Writable } from "stream";
 import { PrismaClient } from "@prisma/client";
+import { Workbook } from "exceljs";
+import { createWriteStream, createReadStream, WriteStream } from "fs";
 
 const prisma = new PrismaClient({
   log: ["query"],
 });
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return "Hello World!";
-  }
-
   async getMovies() {
-    for await (const movie of this.streamMovies(100)) {
-      console.log(movie);
-    }
+    return this.streamMovies(1000);
   }
 
-  streamMovies(batchSize: number) {
+  streamMovies(batchSize: number): Readable {
     let cursorId = undefined;
 
     return new Readable({
@@ -43,5 +39,40 @@ export class AppService {
         }
       },
     });
+  }
+
+  createWorksheet(workbook: Workbook) {
+    const worksheet = workbook.addWorksheet("Movies");
+
+    worksheet.columns = [
+      { header: "ID", key: "id", width: 10 },
+      { header: "Title", key: "title", width: 30 },
+      { header: "Created_at", key: "created_at", width: 10 },
+      { header: "Updated_at", key: "updated_at", width: 10 },
+      { header: "Description", key: "description", width: 10 },
+      { header: "Release_date", key: "release_date", width: 10 },
+      { header: "Rating", key: "rating", width: 10 },
+    ];
+
+    return worksheet;
+  }
+
+  async getMoviesInFormatExcel(stream: WriteStream) {
+    const workbook = new Workbook();
+    const worksheet = this.createWorksheet(workbook);
+    for await (const movie of this.streamMovies(1000)) {
+      worksheet.addRow(movie);
+    }
+    await workbook.xlsx.write(stream);
+  }
+
+  async getMoviesInFormatCSV(stream: WriteStream) {
+    const workbook = new Workbook();
+    const worksheet = this.createWorksheet(workbook);
+
+    for await (const movie of this.streamMovies(10000)) {
+      worksheet.addRow(movie);
+    }
+    await workbook.csv.write(stream);
   }
 }
